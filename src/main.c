7ba8fd9ee454dedfa47f87605e5600728e3ea7d9
tvaldig <timotiusvivaldi@gmail.com>
2023-11-04 13:33:38 +0700

    fix(main) : input before session sementara

--- src/main.c
+++ src/main.c
@@ -2,21 +2,54 @@
 #include "boolean.h"
 #include "COMMANDS/welcomeMenu/welcomemenu.h"
 #include "COMMANDS/start/start.h"
+#include "COMMANDS/invalidCommand/invalidCommand.h"
+#include "COMMANDS/help/help.h"
 #include "ADT/ADT.h"
 
 int main()
 {
-    Penyanyi P[100];
-    TabInt ListDefault;
-    MakeEmpty(&ListDefault);
+    boolean program = false;
     char command[100];
     welcomeMenu();
-    STARTCOMMAND();
-    ConvertWordToString(&currentCommand, command);
-    if (IsStringEqual(command, "START"))
-    {
-        startFunction(&ListDefault);
+    while(!program){
+        STARTCOMMAND();
+        ConvertWordToString(&currentCommand, command);
+        if (IsStringEqual(command, "START"))
+        {
+            if(checkCommand(command, false)){
+                program = true;
+                printf(" start berhasil dijalankan\n");
+            } else {
+                wrongCommand();
+            }
+        }
+        else if (IsStringEqual(command, "LOAD")){
+            if (checkCommand(command, false))
+            {
+                program = true;
+                printf("load berhasil dijalankan\n");
+            }
+            else
+            {
+                wrongCommand();
+            }
+        }
+        else if (IsStringEqual(command, "HELP"))
+        {
+            if (checkCommand(command, false))
+            {
+                help(true);
+                program = true;
+            }
+            else
+            {
+                wrongCommand();
+            }
+        } else {
+            unknownCommand();
+        }
     }
+    
 
     
 }
