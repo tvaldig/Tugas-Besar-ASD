00f253f17b870cf88b43fccbfe977d6df0ba736f
david-dewanto <18222027@mahasiswa.itb.ac.id>
2023-11-06 23:08:07 +0700

    UPDATE makefile

---
+++ src/testing.mk
@@ -0,0 +1,7 @@
+all: run clean
+
+test: main.c COMMANDS/welcomeMenu/welcomemenu.c COMMANDS/start/start-test.c ADT/MesinKata/mesinkata.c ADT/MesinKarakter/mesinkarakter.c ADT/List/array.c COMMANDS/invalidCommand/invalidCommand.c COMMANDS/help/help.c ./ADT/Queue/circular_queue.c ./ADT/Stack/stack.c ./ADT/List/arraydin.c ./ADT/Linked-List/linked_list.c ./ADT/Map/map.c ./ADT/Set/set.c 
+	gcc -o main main.c COMMANDS/welcomeMenu/welcomemenu.c COMMANDS/start/start-test.c ADT/MesinKata/mesinkata.c ADT/MesinKarakter/mesinkarakter.c ADT/List/array.c COMMANDS/invalidCommand/invalidCommand.c COMMANDS/help/help.c ./ADT/Queue/circular_queue.c ./ADT/Stack/stack.c ./ADT/List/arraydin.c ./ADT/Linked-List/linked_list.c ./ADT/Map/map.c ./ADT/Set/set.c 
+
+run: test
+	./main.exe
