0b4bdf70981d8f433ee7e7e01b179d17fe351b07
tvaldig <timotiusvivaldi@gmail.com>
2023-11-09 10:36:05 +0700

    tes

--- src/main.c
+++ src/main.c
@@ -20,29 +20,29 @@ int main()
         if (checkCommand(command, inSession))
         {
             if(IsStringEqual(command, "START;")){
-                Word fname = {"../save/new.txt",15};
-                Word np = {"BLACKPINK", 9};
-                Word p = {"BORN PINK", 9};
-                Word s = {"SHUT DOWN", 9};
+                Word fname = {"../save/new.txt", 15};
                 startLoadFunction(fname, false);
                 printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
-                ListSingers(ArrayPenyanyi, ArrayPenyanyi.Neff);
-                ListAlbums(mapAlbum, np);
-                ListSongs(SetLagu, p, s);
+                inSession = true;
             }else if(IsStringEqual(command, "LOAD")){
                 STARTCOMMAND();
-                if(currentCommand.TabWord[currentCommand.Length-1] == ';'){
-                    currentCommand.TabWord[currentCommand.Length-1] = '\0';
-                    currentCommand.Length--;
+                if(IsCommandWithSemicolon(currentCommand)){
+                    handleSemicolon(currentCommand);
                     Word direktori = {"../save/",8};
                     startLoadFunction(ConcatString(direktori, currentCommand), true);
+                    
                 }else{
                     unknownCommand();
                 }
-                
-
-            }else if(IsStringEqual(command, "HELP")) {
+            }else if(IsStringEqual(command, "HELP;")) {
                 help(inSession);
+            } else if(IsStringEqual(command, "LIST")){
+                STARTCOMMAND();
+                if(currentCommand.TabWord[currentCommand.Length - 1] == ';'){
+                    currentCommand.TabWord[currentCommand.Length - 1] = '\0';
+                    currentCommand.Length--;
+                    
+                }
             }
         }
     
