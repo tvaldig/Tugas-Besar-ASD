96285834504ec7e5225bc12ad61f85a8762599a0
tvaldig <timotiusvivaldi@gmail.com>
2023-11-09 10:06:31 +0700

    main masih testing

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "COMMANDS/startload/startload.h"
 #include "COMMANDS/invalidCommand/invalidCommand.h"
 #include "COMMANDS/help/help.h"
+#include "COMMANDS/list/list.h"
 #include "ADT/ADT.h"
 
 
@@ -11,7 +12,7 @@ int main()
 {
     boolean program = false;
     char command[100];
-    welcomeMenu();
+    //welcomeMenu();
     while(!program){
         printf(">>");
         STARTCOMMAND();
@@ -20,9 +21,14 @@ int main()
         {
             if(IsStringEqual(command, "START;")){
                 Word fname = {"../save/new.txt",15};
+                Word np = {"BLACKPINK", 9};
+                Word p = {"BORN PINK", 9};
+                Word s = {"SHUT DOWN", 9};
                 startLoadFunction(fname, false);
                 printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
-
+                ListSingers(ArrayPenyanyi, ArrayPenyanyi.Neff);
+                ListAlbums(mapAlbum, np);
+                ListSongs(SetLagu, p, s);
             }else if(IsStringEqual(command, "LOAD")){
                 STARTCOMMAND();
                 if(currentCommand.TabWord[currentCommand.Length-1] == ';'){
