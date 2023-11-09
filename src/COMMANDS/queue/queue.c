f9faa6f1a671da36740f500915a18e9d3bc78eea
tvaldig <timotiusvivaldi@gmail.com>
2023-11-09 23:21:51 +0700

    queue song + gueue clear

--- src/COMMANDS/queue/queue.c
+++ src/COMMANDS/queue/queue.c
@@ -1,5 +1,26 @@
 #include "queue.h"
 #include "../startload/startload.h"
+#include "../list/list.h"
+
+Word GetJudulLagu(Set SetLagu[], Word namaalbum, int idlagu)
+{
+    boolean found = false;
+    Word NotFound = {"NOTFOUND",8};
+    int idAlbum = GetIdAlbum(namaalbum), i = 0;
+    Set S = SetLagu[Value(mapAlbum, idAlbum)];
+    while(i < S.Count && !found){
+        if(S.AlbumLagu[i].IdLagu == idlagu){
+            found = true;
+        } else{
+            i++;
+        }
+    }
+    if(found){
+        return S.AlbumLagu[i].JudulLagu;
+    } else {
+        return NotFound;
+    }
+}
 void queueFunction(){
     printf("ini untuk handle queue function\n");
 }
@@ -8,4 +29,27 @@ void ClearQueue(Queue *q){
     IDX_HEAD(*q) = IDX_UNDEF;
     IDX_TAIL(*q) = IDX_UNDEF;
     printf("Queue berhasil dikosongkan.");
+}
+
+void QueueSong(Queue *q){
+    char namapenyanyi[100];
+    char judullagu[100];
+    ListSingers(ArrayPenyanyi, ArrayPenyanyi.penyanyi);
+    printf("Masukkan Nama Penyanyi : ");
+    STARTCOMMAND(false);
+    handleSemicolon(currentCommand);
+    ConvertWordToString(&currentCommand, namapenyanyi);
+    ListAlbums(mapAlbum, currentCommand);
+    printf("Masukkan Nama Album yang dipilih :");
+    STARTCOMMAND(false);
+    handleSemicolon(currentCommand);
+    Word NamaAlbum = currentCommand;
+    ListSongs(SetLagu, currentCommand);
+    printf("Masukkan ID Lagu yang dipilih: ");
+    STARTCOMMAND(false);
+    handleSemicolon(currentCommand);
+    int idlagu = ConvertWordToInt(currentCommand);
+    Word JudulLaguWord = GetJudulLagu(SetLagu, NamaAlbum, idlagu);
+    ConvertWordToString(&JudulLaguWord, judullagu);
+    printf("Berhasil menambahkan lagu %s oleh %s ke queue.", judullagu, namapenyanyi);
 }
