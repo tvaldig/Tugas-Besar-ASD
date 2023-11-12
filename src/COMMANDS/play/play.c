65804e51a05d866c71198421537f4c78ed816897
Salt-E <alvinfadhillah@gmail.com>
2023-11-12 19:36:29 +0700

    12 nov

---
+++ src/COMMANDS/play/play.c
@@ -0,0 +1,18 @@
+#include "play.h"
+
+void PlaySong (Queue *q, Stack *s, TabInt ArrPenyanyi){
+
+    CreateQueue(q);
+    CreateEmptyStack(s);
+}
+    
+    
+    
+    // printf("Daftar Penyanyi: \n");
+    // for (int i = 0 ; i < ArrPenyanyi.Neff; i++ ){
+    //     printf("%d. ", (i+1));
+    //     displayWord(ArrPenyanyi.penyanyi[i].namapenyanyi);
+    // }
+    // Word pilihanPenyanyi;
+    // printf("Masukkan Nama Penyanyi yang dipilih : ");
+    
---
+++ src/COMMANDS/play/play.h
@@ -0,0 +1,10 @@
+#ifndef PLAY_H
+#define PLAY_H
+
+#include "../../ADT/ADT.h"
+void PlaySong (Queue *q, Stack *s, TabInt ArrPenyanyi);
+
+void PlayPlaylist (Queue *q, Stack *s);
+
+
+#endif 
