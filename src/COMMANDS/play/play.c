6a529ae58cf3d26508b01eafa24dc1d148d79230
Salt-E <alvinfadhillah@gmail.com>
2023-11-13 13:44:40 +0700

    13nov

--- src/COMMANDS/play/play.c
+++ src/COMMANDS/play/play.c
@@ -14,14 +14,22 @@
 //      }
 // }
 
-void PlaySong (Queue *antrian, Stack *riwayat){
+void PlaySong (Queue *antrian, Stack *riwayat, TabInt ArrPenyanyi, NowPlaying *current){
+    printf("Daftar Penyanyi: \n");
+    for (int i = 0 ; i < ArrPenyanyi.Neff; i++ ){
+        printf("%d. ", (i+1));
+        displayWord(ArrPenyanyi.penyanyi[i].namapenyanyi);
+    }
+    printf("Masukkan Nama Penyanyi yang dipilih : ");
+    STARTCOMMAND(false);
+    handleSemicolon(currentCommand);
+
 
     CreateQueue(antrian);
     CreateEmptyStack(riwayat);
 }
 
-void PlayPlaylist(Queue *antrian, Stack *riwayat, ArrayDin *playlists) {
-    ArrayDin array;
+void PlayPlaylist(Queue *antrian, Stack *riwayat, ArrayDin *playlists, NowPlaying *current) {
     printf("Masukkan ID Playlist: ");
     STARTCOMMAND(false);
     handleSemicolon(currentCommand);
@@ -32,22 +40,28 @@ void PlayPlaylist(Queue *antrian, Stack *riwayat, ArrayDin *playlists) {
     else{
     playlist p = playlists->A[idPlaylist];
     addressnode P = First(p);
+    CreateQueue(antrian);
+    CreateEmptyStack(riwayat);
     while( P != null){
         enqueue(antrian, PENYANYI(P), ALBUM(P), LAGU(P));
         Push(riwayat,PENYANYI(P), ALBUM(P), LAGU(P));
         P=Next(P);
     }
     Reversestack(riwayat);
+    dequeue(antrian, (*current).penyanyi, (*current).album, (*current).lagu);
     printf("Memutar playlist %s", p.namaplaylist.TabWord);
     }
 }
     
     
-    // printf("Daftar Penyanyi: \n");
-    // for (int i = 0 ; i < ArrPenyanyi.Neff; i++ ){
-    //     printf("%d. ", (i+1));
-    //     displayWord(ArrPenyanyi.penyanyi[i].namapenyanyi);
-    // }
-    // Word pilihanPenyanyi;
-    // printf("Masukkan Nama Penyanyi yang dipilih : ");
-    
+void NotPlaying (NowPlaying *current){
+    (*current).penyanyi = IdxUndef;
+    (*current).album = IdxUndef;
+    (*current).lagu = IdxUndef;
+}
+
+boolean isPlaying (NowPlaying current){
+    return current.penyanyi == IdxUndef && 
+    current.album == IdxUndef && 
+    current.lagu == IdxUndef;
+}
--- src/COMMANDS/play/play.h
+++ src/COMMANDS/play/play.h
@@ -3,10 +3,18 @@
 
 #include "../../ADT/ADT.h"
 #include "../startload/startload.h"
+typedef struct
+{
+    IdxType penyanyi;
+    IdxType album;
+    IdxType lagu;
+} NowPlaying;
 
-void PlaySong (Queue *q, Stack *s);
+void PlaySong (Queue *antrian, Stack *riwayat, TabInt ArrPenyanyi, NowPlaying *current);
 
-void PlayPlaylist (Queue *antrian, Stack *riwayat, ArrayDin *playlists);
+void PlayPlaylist (Queue *antrian, Stack *riwayat, ArrayDin *playlists, NowPlaying *current);
 
+void NotPlaying (NowPlaying *current);
 
+boolean isPlaying (NowPlaying current);
 #endif 
