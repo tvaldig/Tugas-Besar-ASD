e434cb88c05f84b57776a2ef6cc83f0c4eac0575
tvaldig <timotiusvivaldi@gmail.com>
2023-11-19 23:55:37 +0700

    fix number generator

--- src/COMMANDS/enhance/enhance.c
+++ src/COMMANDS/enhance/enhance.c
@@ -1,15 +1,25 @@
 #include "enhance.h"
 #include "time.h"
 
+void delaytime()
+{
+    int delay;
+    for (int i = 0; i < 100000999; i++)
+    {
+        delay = i;
+    }
+}
+
 int tauswortheNumberGenerator(int min, int max){
     unsigned int s1 = 13, s2 = 19 , s3 = 21, seed = time(NULL);
     unsigned int mask = 0xffffffffUL;
     int output;
     seed = (seed & mask) << s1 ^ ((seed << s2) & mask);
     seed = (seed & mask) ^ ((seed >> s3) & mask);
-    output = seed % (min+max+1);
+    output = (seed % (max-min)) + min;
     return output;
 }
+
 
 void ENHANCE(){
     PrintArrayDin(playlists);
@@ -23,9 +33,15 @@ void ENHANCE(){
         playlist ply = playlists.A[id];
         displayPlaylist(playlists, id);
         printf("\nRekomendasi Penyanyi dengan Album :\n");
-        for(int i = 0; i < 2; i++){
-            Penyanyi penyanyi = ArrayPenyanyi.penyanyi[tauswortheNumberGenerator(0, ArrayPenyanyi.Neff)];
-            printf("%s - %s", penyanyi.namapenyanyi.TabWord, mapAlbum.Elements[tauswortheNumberGenerator(penyanyi.IdAlbumPertama, penyanyi.jumlahalbum)].AlbumName.TabWord);
+        for(int i = 0; i < 3; i++){
+            Penyanyi P = ArrayPenyanyi.penyanyi[tauswortheNumberGenerator(0, ArrayPenyanyi.Neff)];
+            int idalbumpertama = P.IdAlbumPertama;
+            int jumlahalbum = P.jumlahalbum;
+            Album A = mapAlbum.Elements[tauswortheNumberGenerator(idalbumpertama, jumlahalbum)];
+            delaytime();
+            delaytime();
+            delaytime();
+            printf("%d. %s - %s\n", i+1, P.namapenyanyi.TabWord, A.AlbumName.TabWord);
         }
     }
 
