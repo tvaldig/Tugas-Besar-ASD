8cf6731b8faa26688db1de89d4a38c80f56b2f1d
tvaldig <timotiusvivaldi@gmail.com>
2023-11-20 17:10:38 +0700

    deskripsi mesinkata

--- src/ADT/MesinKata/mesinkata.h
+++ src/ADT/MesinKata/mesinkata.h
@@ -49,40 +49,61 @@ void CopyCommand();
  */
 void CopyCommandNotBlank();
 
-/*
+/*Mesin kata untuk mengakuisisi kata pada sebuah file konfigurasi
+   I.S : currentChar sembarang
+   F.S : currentWord menjadi kata yang telah diakuisi.
 */
 void STARTFROMFILE(char *file);
 
+/*Mesin kata untuk mengakuisisi selanjutnya kata pada sebuah file konfigurasi
+   I.S : currentChar sembarang
+   F.S : currentWord menjadi kata yang telah diakuisi.
+*/
 void ADVOnEnter(boolean OnBlank);
 
+/*Melakukan salin kata pada file dengan BLANK
+*/
 void COPYFILE();
 
+/*Melakukan salin kata pada file tanpa BLANK
+ */
 void COPYFILEOnBlank();
 
+/* prosedur untuk melakukan akuisisi kata selanjutnya bila belum berhenti
+*/
 void ADVCONTINUE();
 
+/* prosedur untuk melakukan copy pada semicolon
+ */
 void COPYFILESEMICOLON();
 
+/* prosedur untuk melakukan akuisisi kata selanjutnya pada semicolon
+ */
 void ADVSEMICOLON();
 
+/* prosedur untuk menutup input command
+ */
 void ENDCOMMAND();
 
+/* Mengabaikan satu atau beberapa BLANK
+   I.S. : currentChar sembarang
+   F.S. : currentChar ≠ BLANK atau currentChar = MARK */
 void IgnoreBlankFile();
 
 
-    /* PRIMITIF CONVERTER WORD ATAU STRING*/
-void ConvertWordToString(Word *word, char *output);
+/* PRIMITIF CONVERTER WORD ATAU STRING*/
+void ConvertWordToString(Word *word, char *output); //Convert word menjadi string
 
-boolean IsStringEqual(char str1[], char str2[]);
+boolean IsStringEqual(char str1[], char str2[]); //check string equal
 
-void displayWord(Word w);
+void displayWord(Word w); //print word
 
-int ConvertWordToInt(Word word);
+int ConvertWordToInt(Word word); // Convert word menjadi integer
 
-Word ConcatString(Word input1, Word input2);
+Word ConcatString(Word input1, Word input2); //Concat dua buah word
 
-boolean IsCommandWithSemicolon(Word command);
+boolean IsCommandWithSemicolon(Word command); // fungsi untuk cek apakah command dengan semicolon
 
-void handleSemicolon(Word command);
+void handleSemicolon(Word command);// prosedur untuk menghapus semicolon
 
 #endif
