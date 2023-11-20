6b55a2fc4e6154271209d89e54df72935889f9a6
david-dewanto <18222027@mahasiswa.itb.ac.id>
2023-11-20 20:48:16 +0700

    Makefile Addition in Set + Output Revision

--- src/ADT/Set/driver-set.c
+++ src/ADT/Set/driver-set.c
@@ -8,7 +8,7 @@ int main(){
 
     CreateEmptySet(&driver); // Menginisialisasi Set Kosong
 
-    printf("\nUntuk menghentikan test driver, masukkan input \"-999\" pada input ID Lagu!\n\n");
+    printf("\nUntuk menghentikan test driver, masukkan input \"-999\" pada input nama lagu!\n\n");
     printf("Masukkan nama lagu: ");
     STARTCOMMAND(false);
 
@@ -17,7 +17,7 @@ int main(){
         printf("\nISI SET SAAT INI\n");
         PrintSet(driver);
 
-        printf("\nUntuk menghentikan test driver, masukkan angka -999 pada input ID Lagu!\n");
+        printf("\nUntuk menghentikan test driver, masukkan angka -999 pada input nama lagu!\n");
         printf("Bila ada masukkan dengan nama lagu yang sama maka akan otomatis tidak dimasukkan ke dalam Set! \n\n");
         printf("Masukkan nama lagu: ");
         STARTCOMMAND(false);  
